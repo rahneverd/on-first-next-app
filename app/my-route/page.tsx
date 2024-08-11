f1bb9e46ac176934b3204d2d8b879ccf401e2755
rahneverd <rahneverd@gmail.com>
2024-08-11 20:27:16 +0500

    video 3 done

--- app/my-route/page.tsx
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-  // logic
-  return <div>my route</div>;
-}
-
-export default myFunction;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { redirect } from 'next/navigation';
 
 function page() {
-  return <div>page</div>;
+  // return <div>main page</div>;
+  redirect('/dashboard');
 }
 
 export default page;
